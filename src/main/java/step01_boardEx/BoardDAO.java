7aef888a40d8e4c6bc9595373f4865d3e1e2026e
choikyeongjeong <choikyeongjeong@gmail.com>
2023-07-19 17:26:24 +0900

    2023.07.19

--- src/main/java/step01_boardEx/BoardDAO.java
+++ src/main/java/step01_boardEx/BoardDAO.java
@@ -5,56 +5,98 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
-// D(Data) A(Access) O(Object) : 데이터(DB) 접근 객체
+// D(Data) A(Access) O(Object) : 데이터 접근 객체
 public class BoardDAO {
+
+	private BoardDAO(){}
+	private static BoardDAO instance = new BoardDAO();
+	public static BoardDAO getInstance() {
+		return instance;
+	}
 	
-	Connection conn = null;
-	PreparedStatement pstmt = null;
-	ResultSet rs = null;
 	
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
 	
-	//데이터베이스 연결 메서드
+	// 데이터베이스 연결 메서드
 	public void getConnection() {
 		
 		try {
-			
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/MVC1_PRACTICE?serverTimezone=Asia/Seoul", "root", "1234");
-		} 	
-		catch (Exception e) {
-		
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
-	
-	
-	//데이터베이스 연결 자원 해제 메서드
+
+	// 데이터베이스 연결 자원 해제 메서드
 	public void getClose() {
-		
-		if(rs != null) 		try { rs.close(); } 	catch (SQLException e) { e.printStackTrace(); }
-		if(pstmt != null) 	try { pstmt.close(); } 	catch (SQLException e) { e.printStackTrace(); }
-		if(conn != null) 	try { conn.close(); } 	catch (SQLException e) { e.printStackTrace(); }
+		if (rs != null)    try {rs.close();} catch (SQLException e) {e.printStackTrace();}
+		if (pstmt != null) try {pstmt.close();} catch (SQLException e) {e.printStackTrace();}
+		if (conn != null)  try {conn.close();} catch (SQLException e) {e.printStackTrace();}
 	}
 	
 	
-	//게시글 작성 DAO
-	public void insertBoard() {
+	// 게시글 작성 DAO
+	public void insertBoard(BoardDTO boardDTO) {	
 		
-		try {		
+		try {
+			
 			getConnection();
 			
-			String sql = "INSERT INTO BOARD(WRITER, EMAIL, SUBJECT, PASSWORD, CONTENT, READ_CNT, ENROLL_DT)";
-			sql += "VALUES(?, ?, ?, ?, ?, 0, ENROLL_DT)";
-			pstmt = conn.prepareStatement(sql);
+			String sql = "INSERT INTO BOARD(WRITER , EMAIL , SUBJECT , PASSWORD , CONTENT , READ_CNT , ENROLL_DT)";
+				   sql += "VALUES(? , ? , ? , ? , ? , 0 , NOW())";
 			
-			conn.prepareStatement(null);
-		}
-		catch(Exception e) {			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, boardDTO.getWriter());
+			pstmt.setString(2, boardDTO.getEmail());
+			pstmt.setString(3, boardDTO.getSubject());
+			pstmt.setString(4, boardDTO.getPassword());
+			pstmt.setString(5, boardDTO.getContent());
+			pstmt.executeUpdate();
+			
+		} 
+		catch(Exception e) {
 			e.printStackTrace();
-		}
-		finally {		
+		} 
+		finally {
+			getClose();
+		}		
+	}
+	
+	public ArrayList<BoardDTO> getBoardList() {
+		
+		ArrayList<BoardDTO> boardList = new ArrayList<BoardDTO>();
+		
+		try {
+			
+			getConnection();
+			//(참고) SQL Syntax Exception : 쿼리 문법 오류
+			pstmt = conn.prepareStatement("SELECT * FROM BOARD");
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				
+				BoardDTO temp = new BoardDTO();
+				temp.setBoardId(rs.getLong("BOARD_ID"));
+				temp.setWriter(rs.getString("WRITER"));
+				temp.setSubject(rs.getString("SUBJECT"));
+				temp.setReadCnt(rs.getLong("READ_CNT"));
+				temp.setEnrollDt(rs.getDate("ENROLL_DT"));
+				boardList.add(temp);
+			}
+		} 
+		catch(Exception e) {
+			e.printStackTrace();
+		} 
+		finally {
 			getClose();
 		}
+	
+		return boardList;
 	}
 }
--- src/main/webapp/step01_boardEx/bWritePro.jsp
+++ src/main/webapp/step01_boardEx/bWritePro.jsp
@@ -1,5 +1,6 @@
+<%@page import="step01_boardEx.BoardDAO"%>
+<%@page import="step01_boardEx.BoardDTO"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,16 +9,30 @@
 </head>
 <body>
 
-	<%
+	<% 
+	
 		request.setCharacterEncoding("utf-8");
-		request.getParameter("writer");
-		request.getParameter("subject");
-		request.getParameter("email");
-		request.getParameter("password");
-		request.getParameter("content");
 		
+		BoardDTO boardDTO = new BoardDTO();
+		
+		//boardDTO.writer = request.getParameter("writer");
+		//boardDTO.subject = request.getParameter("subject");
+		
+		boardDTO.setWriter(request.getParameter("writer"));
+		boardDTO.setSubject(request.getParameter("subject"));
+		boardDTO.setEmail(request.getParameter("email"));
+		boardDTO.setPassword(request.getParameter("password"));
+		boardDTO.setContent(request.getParameter("content"));
+		
+		BoardDAO.getInstance().insertBoard(boardDTO);
 		
 	%>
+	
+	<script>
+		alert("게시글이 등록되었습니다");
+		//해당 URL로 이동
+		location.href = "bList.jsp";
+	</script>	
 
 </body>
 </html>
